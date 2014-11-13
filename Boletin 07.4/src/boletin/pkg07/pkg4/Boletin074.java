3e338ac3d367e32a4422e64618b849ac436f070e
rpachotome <rpachotome@eniac32.danielcastelao.org>
2014-11-13 08:50:28 +0100

    primer comit

---
+++ Boletin 07.4/src/boletin/pkg07/pkg4/Boletin074.java
@@ -0,0 +1,15 @@
+
+package boletin.pkg07.pkg4;
+
+
+public class Boletin074 {
+    public static void main(String[] args) {
+    Comparador com= new Comparador();
+    String a=com.pedirNombre();
+    double x=com.pedirPeso();
+    String b=com.pedirNombre1();
+    double y=com.pedirPeso1();
+    com.compara(x,y);
+    }
+    
+}
